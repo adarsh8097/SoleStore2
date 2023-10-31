ba3cb3f6887e0c8cb322ac27cc8d5bd78238a0f9
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2023-10-31 15:52:36 +0530

    Update Dispetch.js

--- src/frontend/Dispetch.js
+++ src/frontend/Dispetch.js
@@ -8,6 +8,8 @@ import HomePage from "./HomePage";
      function mesgAlert(){
         alert("Your status update comming soon..!");
      }
+
+    
      
     return(
         <>
@@ -18,8 +20,19 @@ import HomePage from "./HomePage";
             <div className="card">
                 <div className="card-body">
                     <h1 className="disp"> Your Order SuccessFully Dispetch..!</h1>
-                    <div className="disk">Note:Your Order hasbeen Deliver 2-3 working day..!</div>
-                    <br></br> <p className="order-status" onClick={mesgAlert}>Check Your OrderStatus</p>
+                    <div className="disk" style={{color: "gray",fontSize: "1rem",fontWeight: "bold"}}>Note:Your Order hasbeen Deliver 2-3 working day..!</div>
+                    <br></br> <p className="order-status" style={{
+                        color: "rgb(248, 245, 245)",
+                        padding: "1rem",
+                        fontSize: "1rem",
+                        border: "1px solid black",
+                        textAlign: "center",
+                        borderRadius: "2rem",
+                        backgroundColor: "rgb(91, 165, 165)",
+                        fontWeight: "600",
+                        cursor: "pointer",
+                    }} onClick={mesgAlert}>Check Your OrderStatus</p>
+                   <Link to="/"><p className="order" style={{color:"red"}}>Continue Shoping</p></Link>
                 </div>
             </div>
             </div>
