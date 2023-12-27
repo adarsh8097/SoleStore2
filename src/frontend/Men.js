e0f6d19f3c05905d6c80bd5dd3fc07eb0d27d048
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2023-12-27 18:25:02 +0530

    Update Men.js

--- src/frontend/Men.js
+++ src/frontend/Men.js
@@ -170,7 +170,6 @@ function Men(){
             </div>
 
          </div>
-                
         </div>
         <div id="OFFICIAL-MERCHANDISE-parent">
           <div class="collection-heading">OFFICIAL MERCHANDISE</div>
