12591fa974b3575cd2f0965639da4c03740ce235
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2023-12-27 18:18:39 +0530

    Update Dispetch.js

--- src/frontend/Dispetch.js
+++ src/frontend/Dispetch.js
@@ -3,7 +3,7 @@ import './dispetch.css';
 import { Link } from "react-router-dom";
 import HomePage from "./HomePage";
  function dispetch(){
-    // const  cartproductItem = JSON.parse(localStorage.getItem('cartItem')) || [];
+   
 
      function mesgAlert(){
         alert("Your status update comming soon..!");
@@ -13,7 +13,7 @@ import HomePage from "./HomePage";
      
     return(
         <>
-        {/* <h1>This is dispetch Page...!</h1> */}
+      
         <HomePage/>
         <div className="card-container mt-5">
             <div className="row">
@@ -21,7 +21,7 @@ import HomePage from "./HomePage";
                 <div className="card-body">
                     <h1 className="disp"> Your Order SuccessFully Dispetch..!</h1>
                     <div className="disk" style={{color: "gray",fontSize: "1rem",fontWeight: "bold"}}>Note:Your Order hasbeen Deliver 2-3 working day..!</div>
-                    <br></br> <p className="order-status" style={{
+                    <br></br> <Link to="/UserProfile"><p className="order-status" style={{
                         color: "rgb(248, 245, 245)",
                         padding: "1rem",
                         fontSize: "1rem",
@@ -31,7 +31,7 @@ import HomePage from "./HomePage";
                         backgroundColor: "rgb(91, 165, 165)",
                         fontWeight: "600",
                         cursor: "pointer",
-                    }} onClick={mesgAlert}>Check Your OrderStatus</p>
+                    }} >Check Your OrderStatus</p></Link>
                    <Link to="/"><p className="order" style={{color:"red"}}>Continue Shoping</p></Link>
                 </div>
             </div>
