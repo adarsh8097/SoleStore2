a221c17691c9cb442eafd95929ed4f68f14f1a74
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2024-03-12 10:56:51 +0530

    Update HomePage.js

--- src/frontend/HomePage.js
+++ src/frontend/HomePage.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, {  createContext, useContext, useEffect, useState } from "react";
 import './HomePage.css' ;
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, json } from "react-router-dom";
 import{ FaMicrophone, FaMobile, FaSearch } from "react-icons/fa";
 import Rightslide from "./Rightslide";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
-const HomePage =()=> {
 
+const API_BASE_URL = "https://academics.newtonschool.co/api/v1/ecommerce";
+const PROJECT_ID = "8spjkxc7tnxh";
+
+// {userdatatoken,productdata,setProductdata}
+const HomePage =({userdatatoken})=> {
+  userdatatoken = JSON.parse(sessionStorage.getItem('userDetailsToken'));
+  
+  console.log(`UserData Token:${userdatatoken}`);
   const [searchTerm, setSearchTerm] = useState('');
       const [searchproduct, setIsSearchProduct] = useState([]);
-     
-       
+      const [btnToggle, setBtnToggle] = useState(true);
+      const[wishlistData , setWishlistData] = useState([]);
+      const[productdata  ,  setProductdata] = useState([]);
+      const[cartCount , setCartCount] = useState(0);
+
+            //  var btnToggle = true;
       const navigate = useNavigate();
       const fetchData = (result) => {
          try{
@@ -36,7 +49,11 @@ const HomePage =()=> {
   }
     
 
-   
+  let cartproductItem = JSON.parse(localStorage.getItem('productdata')) || [] ;
+  console.log(cartproductItem);
+  const favorateProduct = JSON.parse(localStorage.getItem('favoriteProductData')) || [];
+  console.log(favorateProduct);
+  
       const handleChange=(e)=>{
        setSearchTerm(e.target.value);
       fetchData(searchTerm);
@@ -63,16 +80,22 @@ const HomePage =()=> {
            
         console.log('userdetails-data', userDetail);
         
-        
        
-                 
+       
+        // console.log(userdatatoken); 
              function details(){
-                 if(Object.keys(userDetail).length === 0){
-                      navigate('/LoginPage');
-                  }else{
-                    navigate('/UserProfile');
-                  }
+              const userdatatoken = JSON.parse(sessionStorage.getItem('userDetailsToken') || "{}");
+                console.log(userdatatoken); 
+                
                  
+                 if(Object.keys(userdatatoken).length === 0){
+                navigate('/LoginPage');
+                setBtnToggle(false);
+                 toast.error('Please Login First..!');
+                 }else{
+                  setBtnToggle(false);
+                  navigate('/UserProfile');
+                 }
                } 
        
     
@@ -81,38 +104,162 @@ const HomePage =()=> {
     const alertmessg =()=>{
          setIsClick(!isclick);
         if(isclick){
-         window.alert(" Comming soon..!");
+        toast.success(" Comming soon..!");
         }
 
 
     }
+    
+    // const favorateProduct = JSON.parse(localStorage.getItem('favoriteProductData'))||[];
 
-    const favorateProduct = JSON.parse(localStorage.getItem('wishlist'))||[];
+    // const cartproductItem = JSON.parse(localStorage.getItem('productdata')) || [];
 
-    const cartproductItem = JSON.parse(localStorage.getItem('cartItem')) || [];
 
-     const addtowishList =(product)=>{
-      const favorateProduct = JSON.parse(localStorage.getItem('wishlist'))||[];
-      console.log('product',product);
-      const ifProductAlreadyExecty = favorateProduct.find(favrate => favrate._id === product._id);
-
-      if(!ifProductAlreadyExecty){
-        favorateProduct.push(product);
-        alert("Product added To wishlist Successfully");
-        localStorage.setItem('wishlist',JSON.stringify(favorateProduct));
-        navigate('/AllProduct');
-      }else{
-        alert("Product allready exist in wishlist...");
+const fetchwishlistData = () => {
+  fetch('https://academics.newtonschool.co/api/v1/ecommerce/wishlist',{
+      method: 'GET',
+      headers: {
+          Authorization: `Bearer ${userdatatoken}`,
+          projectId: "8spjkxc7tnxh",
+      },  
+  })
+  .then((resp) =>  resp.json())
+  .then((data) => {
+      if (data.status === "success") {
+        console.log("data product wishlist", data);
+          setWishlistData(data);
+          console.log(data);
+          localStorage.setItem('favoriteProductData', JSON.stringify(data.data.items)); // Store data in localStorage
+          toast.success(data.message);
+      } else {
+          toast.error(data.message);
       }
+  })
+  .catch((error) => {
+      console.error("Error fetching wishlist data:", error);
+  });
 
-      console.log('wishlist');
-      console.log(favorateProduct);
 
+}
+
+
+useEffect(()=>{
+  fetchwishlistData();
+},[]);
+
+console.log("Product Count", cartCount);
+const getData = () => {
+  try {
+    fetch(`https://academics.newtonschool.co/api/v1/ecommerce/cart`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${userdatatoken}`,
+        projectId: "8spjkxc7tnxh",
+      },
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Please Login first');
       }
+      return response.json();
+    })
+    .then((data) => {
+      //  setProductdata(data);
+      console.log(data);
+      
+      if (data.status === "success") {
+        // localStorage.setItem('productdata', JSON.stringify(data.data.items));
+         setProductdata(data.results);
+         setCartCount(data.results);
+        
+      console.log(data);
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+      toast.error(error.message);
+    });
+  } catch (error) {
+    console.log("data not get it", error);
+    toast.error('Failed to fetch data. Please try again later.');
+  }
+}
+
+
+useEffect(() =>{
+  getData();
+}, []);
+
+console.log("length of Product",);
+
+//     //  const addtowishList =(product)=>{
+//     //   const favorateProduct = JSON.parse(localStorage.getItem('wishlist'))||[];
+//     //   console.log('product',product);
+//     //   const ifProductAlreadyExecty = favorateProduct.find(favrate => favrate._id === product._id);
+
+//     //   if(!ifProductAlreadyExecty){
+//     //     favorateProduct.push(product);
+//     //     alert("Product added To wishlist Successfully");
+//     //     localStorage.setItem('wishlist',JSON.stringify(favorateProduct));
+//     //     navigate('/AllProduct');
+//     //   }else{
+//     //     alert("Product allready exist in wishlist...");
+//     //   }
+
+//     //   console.log('wishlist');
+//     //   console.log(favorateProduct);
+
+//     //   }
+//     // const[wishlist , setWishlist] = useState('');
+     const[datawish , setdataWish] = useState([]);
+    const  addtowishList = async(productId) => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/wishlist/`,{
+          method: 'PATCH',
+          headers: {
+            Authorization: `Bearer ${userdatatoken}`,
+            projectId: PROJECT_ID,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ productId }),
+        });
+        if (!response.ok) {
+          throw new Error('Failed to add product to wishlist');
+        }
+        const data = await response.json();
+        if (data.status === "success") {
+          // setSearchTerm(data);
+          setdataWish(data);
+          console.log("wishlist data",data);
+          fetchwishlistData();
+          // favorateProduct();
+          toast.success(data.message);
   
+        } else {
+           toast.error(data.message);
+        }
+      } catch (error) {
+        // toast.error(error.message);
+        console.error("Error:", error);
+        // toast.error("Failed to add product to wishlist");
+      
+      }
+    };
+  
+
+    //  console.log(wishlist);
+     useEffect(()=>{
+      addtowishList();
+     },[]);
+
+   
      
     return(
         <>
+        <ToastContainer/>
          <header>
          <div data-v-3ee02bb2="" id="topbar" class="topbar">
           <div data-v-3ee02bb2="" class="">
@@ -127,7 +274,8 @@ const HomePage =()=> {
                       <NavLink data-v-3ee02bb2="" to="/Men" activeClassName="" >MEN </NavLink>
                       </li>
                        <li data-v-3ee02bb2="" class=" remo">
-                        <Link data-v-3ee02bb2="" to="#" onClick={alertmessg} >KIDS </Link>
+                        {/* <Link to="./MyFilterProduct">KIDS </Link> */}
+                        <NavLink data-v-3ee02bb2="" to="/MyFilterProduct" activeClassName="" >KIDS </NavLink>
                         </li>
                         </ul>
                         </nav>
@@ -151,18 +299,19 @@ const HomePage =()=> {
 
              <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-light fixed-top navigation-2">
                <span className="navbar-brand" id="logo-img-div">
-                     <Link to="/"><img src="stor-logo.jpg" alt="dp"/></Link> 
+                     <Link to="/"><img src="https://mir-s3-cdn-cf.behance.net/user/276/f9c93b3204855.5a78b3b1aa150.png" alt="dp"/></Link> 
                  </span>
                    
-                   <span class="bar-side"> <Rightslide /></span> 
+                   {/* <span class="bar-side"> <Rightslide /></span>  */}
         
               
            <div className="drop-item">
-                <div class="dropdown">
-                    < span type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
+               <div class="dropdown">
+               <Link to="/MyFilterProduct">
+                  < span type="button" className="btn">
                     TOPWEAR
-                    </span>
-                    <ul class="dropdown-menu">
+                    </span></Link>
+                    {/* <ul class="dropdown-menu">
                         <li><Link class="dropdown-item" to="#">Tops New</Link></li>
                         <li><Link class="dropdown-item" to="#">Shirts</Link></li>
                         <li><Link class="dropdown-item" to="#">Co-ord Sets</Link></li>
@@ -172,11 +321,11 @@ const HomePage =()=> {
                         <li><Link class="dropdown-item" to="#">Sweatshirts & Sweaters New</Link></li>
                         <li><Link class="dropdown-item" to="#">Hoodies & Jackets</Link></li>
                         <li><Link class="dropdown-item" to="#">Dresses</Link></li>
-                    </ul>
+                    </ul> */}
                  </div>
 
                 <div class="dropdown">
-                    <span type="button" class="btn  dropdown-toggle" data-bs-toggle="dropdown">
+                    <span type="button" class="btn  dropdown-toggle" data-bs-toggle="dropdown" onClick={alertmessg}>
                     BOTTOMWEAR
                     </span>
                     <ul class="dropdown-menu">
@@ -191,7 +340,7 @@ const HomePage =()=> {
                     </ul>
                 </div>
                 <div class="dropdown">
-                    <span type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
+                    <span type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" onClick={alertmessg}>
                     SHOES & ACCESSORIES
                     </span>
                     <ul class="dropdown-menu">
@@ -203,7 +352,7 @@ const HomePage =()=> {
                     </ul>
                 </div>
                 <div class="dropdown">
-                    <span type="button" class="btn  dropdown-toggle" data-bs-toggle="dropdown">
+                    <span type="button" class="btn  dropdown-toggle" data-bs-toggle="dropdown" onClick={alertmessg}>
                     SHOP BY THEMES
                     </span>
                 
@@ -218,7 +367,8 @@ const HomePage =()=> {
                     </ul>
                 </div>
                 <div className="dropdown">
-                <Link to="https://www.thesouledstore.com/membership"><span type="button" class="btn">
+                  <Link to="/MyFilterProduct">
+               <span type="button" class="btn">
                 MEMBERSHIP
                  </span></Link>
                
@@ -228,8 +378,7 @@ const HomePage =()=> {
                         
                      <div className="search-container" >
                      
-                        { isSearch &&
-                         <div className="search-input search-input-icons" >
+                        { isSearch && <div className="search-input" >
                          <input type="search" 
                           id="search" 
                           placeholder="Search Product...?" 
@@ -247,15 +396,13 @@ const HomePage =()=> {
                            
                           </div> 
                           
-                           } 
+                          }  
 
                      </div>
 
                    
           
-                         <div class="searchbutton" 
-                         onClick={searchpro}
-                         >
+                         <div class="searchbutton" onClick={searchpro}>
                             <span class="search-input-icons" style={{cursor:"pointer"}} onClick={ handleSearch }>
                               <FaSearch /> 
                             </span>
@@ -264,27 +411,31 @@ const HomePage =()=> {
                           <div class="searchbutton" onClick={details}>
                          <span className="user-Sign-symbol">
                      
-                          <span data-v-3ee02bb2="" class="hicon fa fa-user-o"></span>
+                          <span data-v-3ee02bb2="" class="hicon fa fa-user-o" value={btnToggle}>
+                          
+                          </span>
                         </span>
                         </div>
                      
-                        <div class="wishbutton" >
+                        <div class="searchbutton" >
                       
                        <Link to="/WishlistPage"> 
                        <span className="user-Sign-symbol">
-                        
-                            <span data-v-3ee02bb2="" class="headercart hicon fa fa-heart-o" ></span>
+                        <span data-v-3ee02bb2="" class="headercart hicon fa fa-heart-o" >
+                         
                         </span>
-                        <div class="sc-11rr3rd-0 iqxmt7-0 hoFBBP goNsGa">{favorateProduct.length}</div>
+                        </span>
+                        <div class="sc-11rr3rd-0 iqxmt7-0 hoFBBP goNsGa">
+                          {/* {favorateProduct.length} */}
+                          {userdatatoken?favorateProduct.length:0}
+                          </div>
                         </Link>
-                       
+                        </div>
                      {/*  */}
-                     </div>
-                     <div class="searchbutton" >
+                        <div class="searchbutton" >
                        <Link to="/CartProductPage"> <span className="user-Sign-symbol">
-
-
-                            < svg data-v-3ee02bb2="" xmlns="http://www.w3.org/2000/svg" width="25" height="23" viewBox="0 0 48 48" class="headercart" style= {{stroke: "rgb(114, 107, 107); fill: rgb(34, 32, 32)", strokeWidth: "1.4", webkitFontSmoothing:"antialiased"}}>
+                         
+                            <svg data-v-3ee02bb2="" xmlns="http://www.w3.org/2000/svg" width="25" height="23" viewBox="0 0 48 48" class="headercart" style= {{stroke: "rgb(114, 107, 107); fill: rgb(34, 32, 32)", strokeWidth: "1.4", webkitFontSmoothing:"antialiased"}}>
                             <path data-v-3ee02bb2="" xmlns="http://www.w3.org/2000/svg" d="M43,46H5c-2.209,0-4-1.791-4-4l4-24c0.678-3.442,2.668-4,4.877-4h2.652  C14.037,7.052,18.602,2,24,2s9.963,5.052,11.471,12h2.652c2.209,0,4.199,0.558,4.877,4l4,24C47,44.209,45.209,46,43,46z M24,4  c-4.352,0-8.045,4.178-9.418,10h18.837C32.045,8.178,28.353,4,24,4z M41,18c-0.308-1.351-0.957-2-2.37-2h-2.828  C35.925,16.976,36,17.975,36,19c0,0.552-0.447,1-1,1s-1-0.448-1-1c0-1.027-0.069-2.031-0.201-3H14.201C14.07,16.969,14,17.973,14,19  c0,0.552-0.447,1-1,1s-1-0.448-1-1c0-1.025,0.075-2.024,0.197-3H9.369C7.957,16,7.309,16.649,7,18L3,42c0,1.104,0.896,2,2,2h38  c1.104,0,2-0.896,2-2L41,18z" shape-rendering="auto"></path> 
                             <line data-v-3ee02bb2="" x1="5" y1="32" x2="44" y2="32" style={{strokeWidth: "2"}}>
                               </line>
@@ -292,27 +443,29 @@ const HomePage =()=> {
                         </span>
                     
                        
-                        <div class="sc-11rr3rd-0 iqxmt7-0 hoFBBP goNsGa">{cartproductItem.length}</div>
+                        <div class="sc-11rr3rd-0 iqxmt7-0 hoFBBP goNsGa">
+                          {userdatatoken?cartproductItem.length:0}
+                          </div>
                         </Link>
                      </div>
                  
              </nav> 
              
       <div className="container-fluid">
-       <div className="collection-component col-sm-12 row">
+       <div className="collection-component col-sm-12 row" style={{gap:"30px",justifyContent:"center"}}>
       
        { searchproduct ? ( searchproduct.map((product)=>(
-       <div className="card product-card col-sm-12 mt-3">
-       <div data-v-2d5b3c05="" class="wishlistIcon pl-1 pb-2 pr-1 wishlist" onClick={()=> addtowishList(product)} title="add to Wishlist"></div>
+       <div className="card product-card col-sm-12 mt-3" style={{height:"390px"}}>
+       <div data-v-2d5b3c05="" class="wishlistIcon pl-1 pb-2 pr-1 wishlist" onClick={()=> addtowishList(product._id)} title="add to Wishlist"> <i class="fa fa-heart-o" style={{fontSize:"20px", marginLeft:"100%",cursor:"pointer", position:"relative", zIndex:"-3", overflowY:"2"}}></i></div>
       <div className="card-head">
-       <div className="img-card-div" key={product._id} >
+       <div className="img-card-div" key={product._id}>
        <Link to={`/Allproduct/${product._id}`}>
           <img src={product.displayImage} alt={product.alt}/>
            </Link>
          </div>
        <h6 className="product-price">Price:{product.price}</h6>
           <div className="card-info">
-            {product.name}
+            <p style={{fontSize:"15px"}}>{product.name}</p>
          </div>
            </div>
          </div>
