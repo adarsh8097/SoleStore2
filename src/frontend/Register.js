55a3908bfcb426dbc8757653270fa02274eb3bd6
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2024-03-12 11:03:21 +0530

    Update Register.js

--- src/frontend/Register.js
+++ src/frontend/Register.js
@@ -2,136 +2,110 @@ import React, { useState } from "react";
 import './Register.css';
 import { Link, useNavigate } from "react-router-dom";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
-import HomePage from "./HomePage";
 import FooterPage from "./FooterPage";
-function Register(){
-     
-  const[userfirstName, setUerFirstName] = useState("");
-  const[userLastName, setUserLastName] = useState("");
-  const[email,setEmail] = useState("");
-  const[password,setPassword] = useState("");
-  
-  const[phone,setPhone] = useState("");
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+function Register() {
+  const [userFirstName, setUserFirstName] = useState("");
+  const [userLastName, setUserLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
   const navigate = useNavigate();
 
-  const submitForm =()=>{
-    
-    fetch("https://academics.newtonschool.co/api/v1/user/signup",
-    {
-     method:"POST",
-        headers:
-        {
-            "Content-Type": "application/json",
-            projectId:"8spjkxc7tnxh",
-        },
-        body:JSON.stringify({
-            name: userfirstName,
-            last_name: userLastName,
-            email: email,
-            password: password,
-          
-            phonenumber: phone,
-            appType:"ecommerce",
-
-        }),
-
-    }).then((resp) =>{
-        if(resp.status >= 400){
-            new Error("data not found");
+  const submitForm = (e) => {
+    e.preventDefault();
+    fetch("https://academics.newtonschool.co/api/v1/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        projectId: "8spjkxc7tnxh",
+      },
+      body: JSON.stringify({
+        name: userFirstName,
+        last_name: userLastName,
+        email: email,
+        password: password,
+        phonenumber: phone,
+        appType: "ecommerce",
+      }),
+    })
+      .then((resp) => {
+        if (resp.status >= 400) {
+          throw new Error("data not found");
         }
         return resp.json();
-
-    }).then((data)=>{
-
-      sessionStorage.setItem("userDetails",JSON.stringify(data));
-        console.log('data',data);
-         console.log("data Success");
-     
-        if(userfirstName && userLastName && email && password && phone !== 0){
-          if(!email.includes('@')){
-            alert('Email must contain "@" symbol');
-          } else{
-          alert('please Login');
+      })
+      .then((data) => {
+        if (data.status === 'success') {
+          sessionStorage.setItem("userDetails", JSON.stringify(data));
+          toast.success("Register SuccessFully")
+          alert("Register SuccessFully");
+         
           navigate('/LoginPage');
-          }
+        } else {
+          alert(data.message);
+          toast.error(data.message);
         }
-        else{
-          alert('please Provide all thing..!');
-      navigate('/Register');  
-      } 
-    })
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.log("data is not found...!", error);
+      });
+  };
 
-    
-    .catch((error)=>{
-        alert.message(error.message);
-        console.log("data is not found...!",error);
-
-    });
-    
-    
-   }
-
-
-
-
-
-   const registerfun=()=>{
+  const handleRegisterClick = () => {
     alert("Server side issue please try after sometime...!");
-}
+  }
 
-    return(
-
-        <>
-        <HomePage/>
-          {/* <!-- register --> */}
-          <div id="myModal" class="modal">
-          <div class="modal-content">
-        <span class="close">&times;</span>
-        <p id="modal-msg">Modal content goes here.</p>
-      </div>
-    </div>
-    <div id="page-container">
-      <div id="login-container">
-        <div id="main-login-content-container">
-          <div id="main-login-content-container-child1">
-            <h3>Login with SOULE-Store Fashions</h3>
-          </div>
-          <span className="header">SignUp Page</span>
-     <div id="login-content">
-        <div id="login-content-inner-div">
-          <div id="register-box">
+  return (
+    <>
+      {/* <!-- register --> */}
+      <ToastContainer/>
+      <div id="page-container">
+        <div id="login-container">
+          <div id="main-login-content-container">
+            <div id="main-login-content-container-child1">
+              <h3>Login with SOULE-Store Fashions</h3>
+            </div>
+            <span className="header">SignUp Page</span>
+            <div id="login-content">
+              <div id="login-content-inner-div">
+                <div id="register-box">
                   <div class="flex-divs">
-                    <a href="#" onClick={registerfun}>
-                      <span><i class="fa-brands fa-square-facebook"><FaFacebook style={{fontSize:"1.5rem",color:"blue"}}/></i> </span>
+                    <a href="#" onClick={handleRegisterClick}>
+                      <span><i class="fa-brands fa-square-facebook"><FaFacebook style={{ fontSize: "1.5rem", color: "blue" }} /></i> </span>
                       <span>Facebook</span>
                     </a>
-                    <a href="#" onClick={registerfun}>
-                      <span><i class="fa-brands fa-goog"><FaGoogle style={{fontSize:"1.2rem",color:"red"}}/></i></span>
+                    <a href="#" onClick={handleRegisterClick}>
+                      <span><i class="fa-brands fa-goog"><FaGoogle style={{ fontSize: "1.2rem", color: "red" }} /></i></span>
                       <span>Google</span>
                     </a>
                   </div>
                   <div>-OR-</div>
-                  <form >
-                    <div style={{display: "flex"}}>
+                  <form onSubmit={submitForm}>
+                    <div style={{ display: "flex" }}>
                       <input
-                        style={{width: "45%"}}
+                        style={{ width: "45%" }}
                         type="text"
                         name="first_name"
                         id="firstName"
-                        required
                         placeholder="First Name *"
-                        onChange={(e)=> setUerFirstName(e.target.value)}
+                        minLength={4}
+                        onChange={(e) => setUserFirstName(e.target.value)}
+                        required
                       />
-                      <div style={{width: "45%"}}></div>
+                      <div style={{ width: "45%" }}></div>
                       <input
-                        style={{width: "45%"}}
+                        style={{ width: "45%" }}
                         type="text"
                         name="last_name"
                         id="lastName"
                         required
                         placeholder="Last Name *"
-                        onChange={(e)=>setUserLastName(e.target.value)}
+                        minLength={4}
+                        onChange={(e) => setUserLastName(e.target.value)}
                       />
                     </div>
                     <input
@@ -140,8 +114,8 @@ function Register(){
                       id="email"
                       required
                       placeholder="Email *"
-                    
-                      onChange={(e)=>setEmail(e.target.value)}
+                      minLength={4}
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                     <input
                       type="password"
@@ -149,9 +123,9 @@ function Register(){
                       id="password"
                       required
                       placeholder="Password *"
-                      onChange={(e)=>setPassword(e.target.value)}
+                      minLength={4}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
-                    
                     <input
                       type="tel"
                       name="phone"
@@ -160,13 +134,13 @@ function Register(){
                       id="phoneNumber"
                       minLength={10}
                       maxLength={10}
-                      onChange={(e)=>setPhone(e.target.value)}
+                      onChange={(e) => setPhone(e.target.value)}
                     />
-                    <div
-                      style={{  display: "flex",
-                        justifyContent: "space-between",
-                        boxSizing: "border-box"}}
-                     >
+                    <div style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      boxSizing: "border-box"
+                    }}>
                       <label for="Gender">Gender</label>
                       &nbsp;
                       <label for="male">Male</label>
@@ -176,24 +150,24 @@ function Register(){
                       <label for="other">Other</label>
                       <input type="radio" name="gender" value="other" />
                     </div>
-                    <button type="button" id="form-register-btn" onClick={submitForm}>Submit</button>
+                    <button type="submit" id="form-register-btn">Submit</button>
                     &nbsp;
                     <div>
                       <span>Already a Customer ? </span>
                       <span>
                         <Link to="/LoginPage">Login</Link>
-                        </span >
+                      </span>
                     </div>
                   </form>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-     </div>
-   </div>
-  </div>
-  <FooterPage/>
-     </>
-    )
+      </div>
+      <FooterPage />
+    </>
+  )
 }
 
 export default Register;
