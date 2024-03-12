19d7a89d79b07f872f8ee2eb823b0492276df89b
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2024-03-12 11:12:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,66 @@ import Register from './frontend/Register';
 import CheckOut from './frontend/CheckOut';
 import Dispetch from './frontend/Dispetch';
 import UserProfile from './frontend/UserProfile';
+import  MyFilterProduct  from './frontend/MyFilterProduct';
+
+
 
 
 function App() {
+  // const[cartCount , setCartCount] = useState(0);
+  // const[cartItem , setIsCart] = useState([]);
+  // const [productdata, setProductdata] = useState([]);
+//   const[userdatatoken,setUserdatatoken] = useState(null);
+ 
+//   useEffect(()=>{
+//     const token = JSON.parse(sessionStorage.getItem('userDetailsToken'));
+//     console.log("token",token);
+//     setUserdatatoken(token);
+//   },[]);
+
+//   console.log("userToken", userdatatoken);
+
+//   const getData = () => {
+//     try {
+//       fetch(`https://academics.newtonschool.co/api/v1/ecommerce/cart`, {
+//         method: "GET",
+//         headers: {
+//           Authorization: `Bearer ${userdatatoken}`,
+//           projectId: "8spjkxc7tnxh",
+//         },
+//       })
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Please Login first');
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         setProductdata(data);
+//         console.log(data);
+//         if (data.status === "success") {
+//           localStorage.setItem('productdata', JSON.stringify(data.data.items));
+//           setProductdata(data);
+          
+//         console.log(data);
+//           toast.success(data.message);
+//         } else {
+//           toast.error(data.message);
+//         }
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//         toast.error(error.message);
+//       });
+//     } catch (error) {
+//       console.log("data not get it", error);
+//       toast.error('Failed to fetch data. Please try again later.');
+//     }
+//   }
+  
+// useEffect(()=>{
+//   getData();
+// },[]);
   return (
     <div className="App">
       <header className="App-header">
@@ -42,7 +99,7 @@ function App() {
         <Router>
         <Routes>
           {/* <Route path="/" element={<HomePage/>}/> */}
-          <Route path='/HeaderPage' element={<HomePage/>} />
+          <Route path='/HeaderPage' element={<HomePage  />} />
           <Route path='/Slider' element={<Slider/>}/>
           <Route path='/Collection' element ={<Collection/>}/>
           <Route path='/FooterPage' element={<FooterPage/>}/>
@@ -55,12 +112,13 @@ function App() {
           <Route path='/AllWomenCollection' element={<AllWomenCollection/>}/>
           <Route path='/description' element={<Description/>}/>
           <Route path='/WishlistPage' element={<WishlistPage/>}/>
-          <Route path='/CartProductPage' element={<CartProductPage/>}/>
+          <Route path='/CartProductPage' element={<CartProductPage />}/>
           <Route path='/LoginPage' element={<LoginPage/>}/>
           <Route path="/Register" element={<Register/>}/>
           <Route path='/CheckOut' element={<CheckOut/>}/>
           <Route path='/dispetch' element={<Dispetch/>}/>
           <Route path='/UserProfile' element={<UserProfile/>}/>
+          <Route path='/MyFilterProduct' element={<MyFilterProduct/>}/>
         </Routes>
         </Router>
        
