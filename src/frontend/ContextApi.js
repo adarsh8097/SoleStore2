604e5b0085e4f55e1057d94faeb8f472c26e02e7
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2024-03-15 16:14:33 +0530

    Update ContextApi.js

--- src/frontend/ContextApi.js
+++ src/frontend/ContextApi.js
@@ -1,1 +1,27 @@
+import react,{ createContext,useContext,useEffect,useState } from "react";
+
+ export  const MyContext = createContext();
+
+
+export default function MyProvider({ children }){
+    const [value, setValue] = useState('Women');
+    const [wishlistProduct , setWishlistPrduct] = useState([]);
+    const[cartproductItemStore, setCartProductItemStore] = useState([]);
+
+    
+    // useEffect(()=>{
+    //   // const favorateProduct = JSON.parse(localStorage.getItem('favoriteProductData')) || [];
+    //   // console.log(favorateProduct);
+    //     if(favorateProduct){
+    //       setWishlistPrduct(favorateProduct);
+    //     }
+    // },[]);
+  
+    return (
+      <MyContext.Provider value={{ value, setValue,wishlistProduct,setWishlistPrduct,cartproductItemStore,setCartProductItemStore}}>
+        {children}
+      </MyContext.Provider>
+    );
+  };
+
 
