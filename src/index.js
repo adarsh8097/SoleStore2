5cca245c0fc8a0e31c85ea8b261dfdba4e657f29
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2024-03-15 16:29:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+// import $ from 'jquery'; 
+// import Popper from 'popper.js'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import  MyProvider  from './frontend/ContextApi';
 // import { Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <MyProvider>
     <App />
+    </MyProvider>
     
   </React.StrictMode>
 );
